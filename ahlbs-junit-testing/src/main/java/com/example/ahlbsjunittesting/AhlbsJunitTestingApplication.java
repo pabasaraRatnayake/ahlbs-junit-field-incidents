1e51a835a386cffdbbd6777d88135d91157bfc77
Pabasara <pabasara.rathnayake@goodhope.co>
2023-06-26 20:09:12 +0530

    Initial deploy

---
+++ ahlbs-junit-testing/src/main/java/com/example/ahlbsjunittesting/AhlbsJunitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ahlbsjunittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhlbsJunitTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhlbsJunitTestingApplication.class, args);
+	}
+
+}
---
+++ ahlbs-junit-testing/src/test/java/com/example/ahlbsjunittesting/AhlbsJunitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ahlbsjunittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhlbsJunitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
