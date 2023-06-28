8c367467de676a7deda4591badb39d918b989e50
Pabasara <pabasara.rathnayake@goodhope.co>
2023-06-28 10:51:23 +0530

    Updated service and test cases

--- ahlbs-junit-testing/src/main/java/com/example/ahlbsjunittesting/FieldIncidentService.java
+++ ahlbs-junit-testing/src/main/java/com/example/ahlbsjunittesting/FieldIncidentService.java
@@ -2,11 +2,23 @@ package com.example.ahlbsjunittesting;
 
 public class FieldIncidentService {
 
-    //get test method
+    public String getTestMethod() {
+        // Implementation of the get test method
+        return "Get test method";
+    }
 
-    //post test method
+    public String postTestMethod() {
+        // Implementation of the post test method
+        return "Post test method";
+    }
 
-    //update test method
+    public String updateTestMethod() {
+        // Implementation of the update test method
+        return "Update test method";
+    }
 
-    //delete test method
+    public String deleteTestMethod() {
+        // Implementation of the delete test method
+        return "Delete test method";
+    }
 }
--- ahlbs-junit-testing/src/main/java/com/example/ahlbsjunittesting/TestFieldIncident.java
+++ ahlbs-junit-testing/src/main/java/com/example/ahlbsjunittesting/TestFieldIncident.java
@@ -1,13 +1,61 @@
 package com.example.ahlbsjunittesting;
 
-public class TestFieldIncident {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    //create incident
-        //call MIS API
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-    //select incident
-        //call MIS API
+class TestFieldIncidentTest {
 
-    //selete incident
-        //create MIS API
+    private FieldIncidentService fieldIncidentService;
+
+    @BeforeEach
+    public void setUp() {
+        fieldIncidentService = new FieldIncidentService();
+    }
+
+    @Test
+    public void testGetTestMethod() {
+        // Perform the necessary setup for the get test method
+
+        // Call the getTestMethod from FieldIncidentService
+        String result = fieldIncidentService.getTestMethod();
+
+        // Assert the result
+        assertEquals("Get test method", result);
+    }
+
+    @Test
+    public void testPostTestMethod() {
+        // Perform the necessary setup for the post test method
+
+        // Call the postTestMethod from FieldIncidentService
+        String result = fieldIncidentService.postTestMethod();
+
+        // Assert the result
+        assertEquals("Post test method", result);
+    }
+
+    @Test
+    public void testUpdateTestMethod() {
+        // Perform the necessary setup for the update test method
+
+        // Call the updateTestMethod from FieldIncidentService
+        String result = fieldIncidentService.updateTestMethod();
+
+        // Assert the result
+        assertEquals("Update test method", result);
+    }
+
+    @Test
+    public void testDeleteTestMethod() {
+        // Perform the necessary setup for the delete test method
+
+        // Call the deleteTestMethod from FieldIncidentService
+        String result = fieldIncidentService.deleteTestMethod();
+
+        // Assert the result
+        assertEquals("Delete test method", result);
+    }
 }
